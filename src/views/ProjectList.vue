9d0dadf8fcab031c7933aaad051a75f261c47588
Roman Volodin <roman.volodin@gmail.com>
2021-05-18 02:37:05 +0300

    Add fetching project from API

--- src/views/ProjectList.vue
+++ src/views/ProjectList.vue
@@ -1,17 +1,44 @@
 <template>
   <div>
     <h1>Project List</h1>
-    <project-card />
+    <project-card
+      v-for="project in projects"
+      :key="project.id"
+      :project="project"
+    />
   </div>
 </template>
 
 <script>
 import ProjectCard from "@/components/ProjectCard.vue";
+import ProjectService from "@/services/ProjectService.js";
 
 export default {
-  name: "Home",
   components: {
     ProjectCard,
+  },
+  data() {
+    return {
+      projects: [],
+    };
+  },
+  created() {
+    ProjectService.getProjects()
+      .then((response) => {
+        response.data.forEach((project) => {
+          let date = new Date(Date.parse(project.created_at));
+          this.projects.push({
+            id: project.id,
+            title: project.title,
+            date: date.toDateString(),
+            time: date.toLocaleTimeString(),
+          });
+        });
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log(error.response);
+      });
   },
 };
 </script>
